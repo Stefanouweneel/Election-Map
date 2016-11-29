86f56d731233c23faec2c53c9f957708e70f7ed9
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-11-29 15:07:01 +0100

    Adjust styling

--- candidates.js
+++ candidates.js
@@ -6,7 +6,7 @@ var newPolitician = function(name, color) {
 };
 
 var candidateOne = newPolitician("Trump", [132, 17, 11]);
-var candidateTwo = newPolitician("Clinton", [245, 141, 136]);
+var candidateTwo = newPolitician("Clinton", [70, 130, 180]);
 
 candidateOne.electionResults = [5, 1, 7, 17, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 11, 2, 11, 1, 3, 7, 2]
 candidateTwo.electionResults = [4, 2, 4, 38, 22, 3, 3, 1, 28, 15,	8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 27, 1, 2, 11, 2, 3, 1]
